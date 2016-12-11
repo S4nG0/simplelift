699d56f8b97f17deab8f0404a4bba3e4fb6c9c3d
S4nG0 <julien.vdm@gmail.com>
2016-12-11 12:17:37 +0100

    init project

---
+++ wp-content/themes/wallstreet/searchform.php
@@ -0,0 +1,4 @@
+<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+	<input type="text" class="search_widget_input"  name="s" id="s" placeholder="<?php esc_attr_e( "Votre recherche", 'wallstreet' ); ?>" />
+	<input type="submit" id="searchsubmit" class="search_btn" style="" name="submit" value="<?php esc_attr_e( "Go !", 'wallstreet' ); ?>" />
+</form>
