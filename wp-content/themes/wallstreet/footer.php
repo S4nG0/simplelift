25e67977076c9aa1bcc9cc4f1ba6421052c30b09
S4nG0 <julien.vdm@gmail.com>
2016-12-11 18:47:29 +0100

    Further fixes on theme files

--- wp-content/themes/wallstreet/footer.php
+++ wp-content/themes/wallstreet/footer.php
@@ -6,19 +6,19 @@
 	<?php if($current_options['footer_social_media_enabled']==true) { ?>
 				<div class="footer-social-area"><ul class="footer-social-icons">
 					<?php if($current_options['social_media_twitter_link']!='') { ?>
-					<li><a href="<?php echo esc_url( $current_options['social_media_twitter_link']); ?>"><i class="fa fa-twitter"></i></a></li>
+					<li><a href="<?php echo esc_url( $current_options['social_media_twitter_link']); ?>" target="_blank"><i class="fa fa-twitter"></i></a></li>
 					<?php }
 					if($current_options['social_media_facebook_link']!='') { ?>
-					<li><a href="<?php echo esc_url( $current_options['social_media_facebook_link']); ?>"><i class="fa fa-facebook"></i></a></li>
+					<li><a href="<?php echo esc_url( $current_options['social_media_facebook_link']); ?>" target="_blank"><i class="fa fa-facebook"></i></a></li>
 					<?php }					
 					if($current_options['social_media_googleplus_link']!='') { ?>
-					<li><a href="<?php echo esc_url( $current_options['social_media_googleplus_link']); ?>"><i class="fa fa-google-plus"></i></a></li>
+					<li><a href="<?php echo esc_url( $current_options['social_media_googleplus_link']); ?>" target="_blank"><i class="fa fa-google-plus"></i></a></li>
 					<?php }
 					if($current_options['social_media_linkedin_link']!='') { ?>
-					<li><a href="<?php echo esc_url( $current_options['social_media_linkedin_link']); ?>"><i class="fa fa-linkedin"></i></a></li>
+					<li><a href="<?php echo esc_url( $current_options['social_media_linkedin_link']); ?>" target="_blank"><i class="fa fa-linkedin"></i></a></li>
 					<?php }
 					if($current_options['social_media_youtube_link']!='') { ?>
-					<li><a href="<?php echo esc_url( $current_options['social_media_youtube_link']); ?>"><i class="fa fa-youtube"></i></a></li>					
+					<li><a href="<?php echo esc_url( $current_options['social_media_youtube_link']); ?>" target="_blank"><i class="fa fa-youtube"></i></a></li>					
 					<?php } ?>
 				</div></ul>
 				<?php } ?>
@@ -33,8 +33,7 @@
         <div class="row">
 			<div class="col-md-12">
 				<div class="footer-copyright">
-					<p><?php echo $current_options['footer_copyright'];?> 
-				</p>
+					<p>&copy; Blog-musculation - 2016 - <a href="<?php echo get_permalink( get_page_by_title( 'Mentions légales' ) ); ?>">Mentions légales</a></p>
 				</div>
 			</div>
 		</div>
